d290d0e1eeccf19fa155a948de586afab73f06cf
PMG <pattoyo01@gmail.com>
2022-11-02 19:24:37 -0300

    se completa acercade, ed, exp en ang

--- Proyecto2022/src/app/app.module.ts
+++ Proyecto2022/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardsoftComponent } from './componentes/hardsoft/hardsoft.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercadeComponent,
+    EncabezadoComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardsoftComponent,
+    ProyectosComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
